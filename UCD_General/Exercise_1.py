d9cf3b2d11ceb44d475b89ae620aaec4e30ccad3
evijakrutaine <evijeens@hotmail.com>
2021-03-06 12:39:38 +0000

    Tird_Commit_3

--- UCD_General/Exercise_1.py
+++ UCD_General/Exercise_1.py
@@ -14,3 +14,28 @@ result = (savings * growth_multiplier ** 7)
 
 # Print out result
 print( result )
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+avings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of savings and growth_multiplier to year1
+year1 = savings * growth_multiplier
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
